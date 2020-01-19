caf39bcecdf6cbc03f7b8e848472a35534f83441
asbyth <cyronize@gmail.com>
2020-01-19 18:18:03 -0500

    remove test method

--- src/main/java/club/sk1er/patcher/test/BytecodeOutput.java
+++ src/main/java/club/sk1er/patcher/test/BytecodeOutput.java
@@ -1,14 +1,4 @@
 package club.sk1er.patcher.test;
 
-import net.minecraft.client.gui.GuiTextField;
-
 public class BytecodeOutput {
-
-    protected GuiTextField inputField;
-    private static boolean supports;
-    private static boolean enabled;
-
-    public void fuckkkk() {
-        inputField.setMaxStringLength(enabled && supports ? 256 : 100);
-    }
 }
