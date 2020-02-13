9d2299ad25f570e11649609ea499c70d069b3c97
asbyth <cyronize@gmail.com>
2020-02-13 00:21:01 -0500

    lord forgive me for i may have to git ignore this class

--- src/main/java/club/sk1er/patcher/test/BytecodeOutput.java
+++ src/main/java/club/sk1er/patcher/test/BytecodeOutput.java
@@ -1,21 +1,4 @@
 package club.sk1er.patcher.test;
 
-import io.netty.channel.ChannelHandler;
-import net.minecraftforge.fml.common.network.FMLEmbeddedChannel;
-import net.minecraftforge.fml.common.network.FMLOutboundHandler;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.handshake.NetworkDispatcher;
-import net.minecraftforge.fml.relauncher.Side;
-
-public class BytecodeOutput extends FMLEmbeddedChannel {
-
-    public BytecodeOutput(String channelName, Side source, ChannelHandler... handlers) {
-        super(channelName, source, handlers);
-    }
-
-    public void cleanAttributes() {
-        attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(null);
-        attr(NetworkRegistry.NET_HANDLER).set(null);
-        attr(NetworkDispatcher.FML_DISPATCHER).set(null);
-    }
+public class BytecodeOutput {
 }
