f8286ac4197a0e64a314c490b2cd411efee6fe9b
asbyth <cyronize@gmail.com>
2020-03-06 14:11:53 -0500

    remove

--- src/main/java/club/sk1er/patcher/test/BytecodeOutput.java
+++ src/main/java/club/sk1er/patcher/test/BytecodeOutput.java
@@ -1,20 +1,3 @@
 package club.sk1er.patcher.test;
 
-import java.io.IOException;
-
-public class BytecodeOutput {
-
-  public static boolean fuck;
-
-  protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-    if (fuck) {
-      checkHotbarKeys(mouseButton - 100);
-    }
-
-    System.out.println("h");
-  }
-
-  protected boolean checkHotbarKeys(int keyCode) {
-    return false;
-  }
-}
+public class BytecodeOutput {}
