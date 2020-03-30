73e7e8b87850607db28fc9a964ce163ead67af40
asbyth <cyronize@gmail.com>
2020-03-30 10:49:02 -0400

    user moment

---
+++ src/main/java/club/sk1er/container/ContainerMessage.java
@@ -0,0 +1,21 @@
+package club.sk1er.container;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+public class ContainerMessage {
+
+  public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    JOptionPane.showMessageDialog(null,
+        "This is a Forge mod, not an application. Please put this in your mods folder, located inside your Minecraft folder."
+            + "\nIf you don't know how to install a Forge mod, search 'Forge Mod Installation Tutorials' online."
+            + "\nIf you are still lost, join us at discord.gg/sk1er.",
+        "This is not the proper installation method.", JOptionPane.INFORMATION_MESSAGE);
+  }
+}
