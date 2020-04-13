9a699d3f5a8de3da0be3f80fe5cb29e722d849b1
asbyth <cyronize@gmail.com>
2020-04-13 00:24:46 -0400

    startswith instead of contains

--- src/main/java/club/sk1er/patcher/util/chat/ChatHandler.java
+++ src/main/java/club/sk1er/patcher/util/chat/ChatHandler.java
@@ -23,7 +23,7 @@ public class ChatHandler {
             String timeFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern("[hh:mm a]"));
             if (PatcherConfig.compactChat) {
                 String message = event.message.getUnformattedText();
-                if (message.contains("---------") || message.contains("=========")) { // why would a user send this
+                if (message.startsWith("---------") || message.startsWith("=========")) { // why would a user send this
                     return; // die!
                 }
 
