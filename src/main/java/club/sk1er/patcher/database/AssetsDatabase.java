a69c725d7a6bfb5d87e103d1cbaf8cf02aae70f4
Mitchell Katz <Sk1er@Sk1er.club>
2020-04-20 21:06:24 -0400

    add file check

--- src/main/java/club/sk1er/patcher/database/AssetsDatabase.java
+++ src/main/java/club/sk1er/patcher/database/AssetsDatabase.java
@@ -14,6 +14,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -101,7 +102,10 @@ public class AssetsDatabase {
     }
 
     public List<String> getAllNegative() throws IOException {
-        return FileUtils.readLines(new File(dir, "negative_cache.txt"));
+        File file = new File(dir, "negative_cache.txt");
+        if (file.exists())
+            return FileUtils.readLines(file);
+        return new ArrayList<>();
     }
 
     public void saveNegative(Set<String> lines) throws IOException {
