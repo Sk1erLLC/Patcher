9cb62cb12a714e5e1d11af2e7d3d712b0a109f78
asbyth <cyronize@gmail.com>
2020-04-24 14:43:03 -0400

    fix mc-234/z-fighting when digging down

--- src/main/java/club/sk1er/patcher/tweaker/asm/RenderGlobalTransformer.java
+++ src/main/java/club/sk1er/patcher/tweaker/asm/RenderGlobalTransformer.java
@@ -2,15 +2,19 @@ package club.sk1er.patcher.tweaker.asm;
 
 import club.sk1er.patcher.tweaker.transform.PatcherTransformer;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
+
+import java.util.ListIterator;
 
 public class RenderGlobalTransformer implements PatcherTransformer {
 
@@ -37,7 +41,20 @@ public class RenderGlobalTransformer implements PatcherTransformer {
 
             if (methodName.equals("renderClouds") || methodName.equals("func_180447_b")) {
                 methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), patcherCloudRenderer());
-                break;
+            } else if (methodName.equals("preRenderDamagedBlocks")) {
+                ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator();
+
+                while (iterator.hasNext()) {
+                    AbstractInsnNode next = iterator.next();
+
+                    if (next instanceof LdcInsnNode && ((LdcInsnNode) next).cst.equals(-3.0F)) {
+                        if (next.getNext() instanceof LdcInsnNode) {
+                            ((LdcInsnNode) next).cst = -1.0F;
+                        } else {
+                            ((LdcInsnNode) next).cst = -10.0F;
+                        }
+                    }
+                }
             }
         }
     }
