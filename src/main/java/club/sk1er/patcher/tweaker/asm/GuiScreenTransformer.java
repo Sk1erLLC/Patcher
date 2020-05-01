e909656e9da73b47da8d085b5af1d0cc251fe5e5
asbyth <cyronize@gmail.com>
2020-04-30 23:33:22 -0400

    allow for asian keyboards to type in chat

--- src/main/java/club/sk1er/patcher/tweaker/asm/GuiScreenTransformer.java
+++ src/main/java/club/sk1er/patcher/tweaker/asm/GuiScreenTransformer.java
@@ -6,6 +6,7 @@ import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.IntInsnNode;
 import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodInsnNode;
@@ -36,9 +37,51 @@ public class GuiScreenTransformer implements PatcherTransformer {
 
             if (methodName.equals("drawDefaultBackground") || methodName.equals("func_146276_q_")) {
                 methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), cancelBackgroundRendering());
-                break;
+            } else if (methodName.equals("handleKeyboardInput") || methodName.equals("func_146282_l")) {
+                methodNode.instructions.clear();
+                methodNode.localVariables.clear();
+                methodNode.instructions.insert(handleForeignKeyboards());
             }
         }
+    }
+
+    private InsnList handleForeignKeyboards() {
+        InsnList list = new InsnList();
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "org/lwjgl/input/Keyboard", "getEventCharacter", "()C", false));
+        list.add(new VarInsnNode(Opcodes.ISTORE, 1));
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "org/lwjgl/input/Keyboard", "getEventKey", "()I", false));
+        LabelNode ifne = new LabelNode();
+        list.add(new JumpInsnNode(Opcodes.IFNE, ifne));
+        list.add(new VarInsnNode(Opcodes.ILOAD, 1));
+        list.add(new IntInsnNode(Opcodes.BIPUSH, 32));
+        LabelNode ificmpge = new LabelNode();
+        list.add(new JumpInsnNode(Opcodes.IF_ICMPGE, ificmpge));
+        list.add(ifne);
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "org/lwjgl/input/Keyboard", "getEventKeyState", "()Z", false));
+        LabelNode ifeq = new LabelNode();
+        list.add(new JumpInsnNode(Opcodes.IFEQ, ifeq));
+        list.add(ificmpge);
+        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+        list.add(new VarInsnNode(Opcodes.ILOAD, 1));
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "org/lwjgl/input/Keyboard", "getEventKey", "()I", false));
+        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
+            "net/minecraft/client/gui/GuiScreen",
+            "func_73869_a", // keyTyped
+            "(CI)V",
+            false));
+        list.add(ifeq);
+        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+        list.add(new FieldInsnNode(Opcodes.GETFIELD,
+            "net/minecraft/client/gui/GuiScreen",
+            "field_146297_k", // mc
+            "Lnet/minecraft/client/Minecraft;"));
+        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
+            "net/minecraft/client/Minecraft",
+            "func_152348_aa", // dispatchKeypresses
+            "()V",
+            false));
+        list.add(new InsnNode(Opcodes.RETURN));
+        return list;
     }
 
     private InsnList cancelBackgroundRendering() {
