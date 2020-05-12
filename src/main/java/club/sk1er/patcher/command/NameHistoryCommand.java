3a025eeae1b46c03a0efeb99faa3142241f0e57d
asbyth <cyronize@gmail.com>
2020-05-12 12:17:16 -0400

    license

--- src/main/java/club/sk1er/patcher/command/NameHistoryCommand.java
+++ src/main/java/club/sk1er/patcher/command/NameHistoryCommand.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2020 by Sk1er LLC
+ *
+ * All rights reserved.
+ *
+ * Sk1er LLC
+ * 444 S Fulton Ave
+ * Mount Vernon, NY
+ * sk1er.club
+ */
+
 package club.sk1er.patcher.command;
 
 import club.sk1er.mods.core.ModCore;
