a5b28b208f4c776f0ffc10d32a285fc6506fff16
Mitchell Katz <Sk1er@sk1er.club>
2020-08-27 00:44:20 -0400

    Food

--- src/main/java/club/sk1er/patcher/hooks/FontRendererHook.java
+++ src/main/java/club/sk1er/patcher/hooks/FontRendererHook.java
@@ -441,9 +441,11 @@ public final class FontRendererHook {
         int row = page / 16;
         int column = page % 16;
         int glyphWidth = this.fontRenderer.glyphWidth[characterIndex] >>> 4;
-        float charX = (float) (characterIndex % 16 * 16) + glyphWidth;
-        float charY = (float) ((characterIndex & 255) / 16 * 16 + .1);
-        return new Pair<>((row * texSheetDim + charX + .05f) / fontTexWidth, (column * texSheetDim + charY) / fontTexHeight); //16 rows each with a size of 64px
+        float charX = (float) (characterIndex % 16 * 16) + glyphWidth + (.05f * page / 39f );
+        float charY = (float) ((characterIndex & 255) / 16 * 16) + (.05f * page / 39f );
+
+
+        return new Pair<>((row * texSheetDim + charX) / fontTexWidth, (column * texSheetDim + charY) / fontTexHeight); //16 rows each with a size of 64px
     }
 
     /**
