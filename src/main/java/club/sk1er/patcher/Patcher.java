5c23acff93a226f4969f32469167b111f3d193fd
asbyth <cyronize@gmail.com>
2020-09-14 01:14:01 -0400

    change: update vers to 1.3.1

--- src/main/java/club/sk1er/patcher/Patcher.java
+++ src/main/java/club/sk1er/patcher/Patcher.java
@@ -87,7 +87,7 @@ public class Patcher {
     // normal versions will be "1.x"
     // betas will be "1.x+beta-y"
     // rcs will be 1.x+rc-y
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1+beta-1";
 
     private final Logger logger = LogManager.getLogger("Patcher");
 
