f6b6cc1fd948fefed8844b835b398b4a5a50ce68
lego3708 <l3gomindstorms@gmail.com>
2020-12-09 17:44:37 +0000

    Fix: fix shadowed nametag z-fighting. Possible performance impact, however?

--- src/main/java/club/sk1er/patcher/hooks/NameTagRenderingHooks.java
+++ src/main/java/club/sk1er/patcher/hooks/NameTagRenderingHooks.java
@@ -2,14 +2,27 @@ package club.sk1er.patcher.hooks;
 
 import club.sk1er.patcher.config.PatcherConfig;
 import net.minecraft.client.gui.FontRenderer;
+import org.lwjgl.opengl.GL11;
 
 @SuppressWarnings("unused")
 public class NameTagRenderingHooks {
     public static int drawNametagText(FontRenderer fontRenderer, String text, int x, int y, int color) {
+        int i;
         if (PatcherConfig.shadowedNametagText) {
-            return fontRenderer.drawStringWithShadow(text, x, y, color);
+            GL11.glDepthMask(false);
+            i = fontRenderer.renderString(text, x + 1.0F, y + 1.0F, color, true);
+            GL11.glDepthMask(true);
+
+            i = Math.max(i, fontRenderer.renderString(text, x, y, color, false));
+
+            GL11.glColorMask(false, false, false, false);
+            i = Math.max(i, fontRenderer.drawString(text, x, y, color, true));
+            GL11.glColorMask(true, true, true, true);
         } else {
-            return fontRenderer.drawString(text, x, y, color);
+            i = fontRenderer.renderString(text, x, y, color, false);
         }
+
+        return i;
     }
+    
 }
