27e4162a8ceccc6635d8de66719170d96fe91ca5
asbyth <cyronize@gmail.com>
2021-03-02 19:03:06 -0500

    new: fix vanilla crash with updating potion effects

--- src/main/java/club/sk1er/patcher/tweaker/asm/EntityLivingBaseTransformer.java
+++ src/main/java/club/sk1er/patcher/tweaker/asm/EntityLivingBaseTransformer.java
@@ -13,16 +13,9 @@ package club.sk1er.patcher.tweaker.asm;
 
 import club.sk1er.patcher.tweaker.transform.PatcherTransformer;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldInsnNode;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.JumpInsnNode;
-import org.objectweb.asm.tree.LabelNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.tree.TypeInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
+import org.objectweb.asm.tree.*;
+
+import java.util.ListIterator;
 
 public class EntityLivingBaseTransformer implements PatcherTransformer {
     /**
@@ -48,9 +41,36 @@ public class EntityLivingBaseTransformer implements PatcherTransformer {
 
             if (methodName.equals("getLook") || methodName.equals("func_70676_i")) {
                 methodNode.instructions.insert(returnSpecial());
-                break;
+            } else if (methodName.equals("updatePotionEffects") || methodName.equals("func_70679_bo")) {
+                int potionEffectIndex = -1;
+
+                for (LocalVariableNode variable : methodNode.localVariables) {
+                    if (variable.name.equals("potioneffect")) {
+                        potionEffectIndex = variable.index;
+                        break;
+                    }
+                }
+
+                final ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator();
+                while (iterator.hasNext()) {
+                    final AbstractInsnNode next = iterator.next();
+                    if (next instanceof MethodInsnNode && next.getOpcode() == Opcodes.INVOKEINTERFACE && ((MethodInsnNode) next).name.equals("get")) {
+                        methodNode.instructions.insertBefore(next.getNext().getNext().getNext(), checkPotionEffect(potionEffectIndex));
+                        break;
+                    }
+                }
             }
         }
+    }
+
+    private InsnList checkPotionEffect(int potionEffectIndex) {
+        InsnList list = new InsnList();
+        list.add(new VarInsnNode(Opcodes.ALOAD, potionEffectIndex));
+        LabelNode ifnonnull = new LabelNode();
+        list.add(new JumpInsnNode(Opcodes.IFNONNULL, ifnonnull));
+        list.add(new InsnNode(Opcodes.RETURN));
+        list.add(ifnonnull);
+        return list;
     }
 
     private InsnList returnSpecial() {
