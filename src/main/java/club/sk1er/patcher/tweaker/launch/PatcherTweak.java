a35fab51c3e8164a7b7164810b5f07a869eaf04e
asbyth <cyronize@gmail.com>
2021-05-25 07:24:16 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/patcher/tweaker/launch/PatcherTweak.java
+++ src/main/java/club/sk1er/patcher/tweaker/launch/PatcherTweak.java
@@ -2,9 +2,9 @@ package club.sk1er.patcher.tweaker.launch;
 
 import club.sk1er.patcher.tweaker.PatcherTweaker;
 import club.sk1er.patcher.tweaker.other.ModTweaker;
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
-public class PatcherTweak extends EssentialTweaker {
+public class PatcherTweak extends EssentialSetupTweaker {
 
     public static long clientLoadTime;
 
