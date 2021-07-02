0b20569b8f5e917128c859514b9ba6204074afad
LlamaLad7 <l3gomindstorms@gmail.com>
2021-07-02 17:06:03 +0100

    Change: Rework show own nametag so it renders as any other nametag would.

--- src/main/java/club/sk1er/patcher/util/world/render/entity/EntityRendering.java
+++ src/main/java/club/sk1er/patcher/util/world/render/entity/EntityRendering.java
@@ -35,8 +35,6 @@ import org.lwjgl.opengl.GL11;
 public class EntityRendering {
 
     private final Minecraft mc = Minecraft.getMinecraft();
-    private final FontRenderer fontRenderer = mc.fontRendererObj;
-    private final RenderManager renderManager = mc.getRenderManager();
     private boolean shouldMakeTransparent;
 
     @SubscribeEvent
@@ -90,74 +88,5 @@ public class EntityRendering {
             GlStateManager.color(1, 1, 1, 1);
             GlStateManager.enableDepth();
         }
-    }
-
-    /**
-     * Render the nametag above our player.
-     *
-     * @param event {@link RenderLivingEvent.Specials.Pre}
-     */
-    @SubscribeEvent
-    public void renderNametag(RenderLivingEvent.Specials.Pre<EntityPlayerSP> event) {
-        if (PatcherConfig.betterHideGui && mc.gameSettings.hideGUI) {
-            return;
-        }
-
-        final EntityLivingBase entity = event.entity;
-        if (entity instanceof EntityPlayerSP
-            && !((EntityPlayerSP) entity).isSpectator() && !entity.isInvisible()
-            && entity == renderManager.livingPlayer && PatcherConfig.showOwnNametag) {
-            renderTag(entity);
-        }
-    }
-
-    private void renderTag(EntityLivingBase entity) {
-        String name = entity.getDisplayName().getFormattedText();
-        float nametagScale = 0.02666667f;
-        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1f);
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(0, entity.height + 0.5f, 0);
-        GL11.glNormal3f(0, 1, 0);
-        GlStateManager.rotate(-renderManager.playerViewY, 0, 1, 0);
-
-        int xMultiplier = 1; // Nametag x rotations should flip in front-facing 3rd person
-
-        if (mc != null && mc.gameSettings != null && mc.gameSettings.thirdPersonView == 2) {
-            xMultiplier = -1;
-        }
-
-        GlStateManager.rotate(renderManager.playerViewX * xMultiplier, 1, 0, 0);
-        GlStateManager.scale(-nametagScale, -nametagScale, nametagScale);
-
-        if (entity.isSneaking()) {
-            GlStateManager.translate(0, 9.374999f, 0);
-        }
-
-        GlStateManager.disableLighting();
-        GlStateManager.depthMask(false);
-        GlStateManager.disableDepth();
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
-        GlStateManager.disableTexture2D();
-
-        int stringWidth = fontRenderer.getStringWidth(name) >> 1;
-        if (!PatcherConfig.disableNametagBoxes) {
-            GlStateManager.color(0, 0, 0, .25F);
-            GL11.glBegin(GL11.GL_QUADS);
-            GL11.glVertex2d(-stringWidth - 1, -1);
-            GL11.glVertex2d(-stringWidth - 1, 8);
-            GL11.glVertex2d(stringWidth + 1, 8);
-            GL11.glVertex2d(stringWidth + 1, -1);
-            GL11.glEnd();
-        }
-
-        GlStateManager.enableTexture2D();
-        GlStateManager.enableDepth();
-        GlStateManager.depthMask(true);
-        NameTagRenderingHooks.drawNametagText(fontRenderer, name, -stringWidth, 0, entity.isSneaking() ? 553648127 : -1);
-        GlStateManager.enableLighting();
-        GlStateManager.disableBlend();
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.popMatrix();
     }
 }
