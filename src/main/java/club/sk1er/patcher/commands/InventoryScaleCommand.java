b103bfd9801998f54783b717db06973fc1547a6c
asbyth <cyronize@gmail.com>
2021-08-15 10:40:03 -0400

    fix: resolve empty text fields messing up text positions

--- src/main/java/club/sk1er/patcher/commands/InventoryScaleCommand.java
+++ src/main/java/club/sk1er/patcher/commands/InventoryScaleCommand.java
@@ -3,6 +3,10 @@ package club.sk1er.patcher.commands;
 import club.sk1er.patcher.util.chat.ChatUtilities;
 import gg.essential.api.commands.Command;
 import gg.essential.api.commands.DefaultHandler;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Set;
 
 public class InventoryScaleCommand extends Command {
     public InventoryScaleCommand() {
@@ -12,5 +16,11 @@ public class InventoryScaleCommand extends Command {
     @DefaultHandler
     public void handle() {
         ChatUtilities.sendNotification("Patcher", "This command has been moved to /patcher invscale");
+    }
+
+    @Nullable
+    @Override
+    public Set<Alias> getCommandAliases() {
+        return Collections.singleton(new Alias("inventoryscale"));
     }
 }
