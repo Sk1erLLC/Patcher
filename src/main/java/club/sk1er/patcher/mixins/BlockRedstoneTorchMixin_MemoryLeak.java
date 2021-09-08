c2726dff67a1b9256acf1a8306a8f4c616b479f4
asbyth <cyronize@gmail.com>
2021-09-08 06:09:16 -0400

    change: simplify BlockRedstoneTorch mixin

--- src/main/java/club/sk1er/patcher/mixins/BlockRedstoneTorchMixin_MemoryLeak.java
+++ src/main/java/club/sk1er/patcher/mixins/BlockRedstoneTorchMixin_MemoryLeak.java
@@ -4,9 +4,6 @@ import net.minecraft.block.BlockRedstoneTorch;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.Map;
@@ -14,11 +11,5 @@ import java.util.WeakHashMap;
 
 @Mixin(BlockRedstoneTorch.class)
 public class BlockRedstoneTorchMixin_MemoryLeak {
-
-    @Shadow private static Map<World, List<BlockRedstoneTorch.Toggle>> toggles;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void patcher$changeMapType(CallbackInfo ci) {
-        toggles = new WeakHashMap<>();
-    }
+    @Shadow private static Map<World, List<BlockRedstoneTorch.Toggle>> toggles = new WeakHashMap<>();
 }
