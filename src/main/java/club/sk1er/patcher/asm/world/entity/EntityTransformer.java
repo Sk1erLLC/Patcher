1a1df3a4a092f3f03d3e1c817b4f1feccb3e1346
asbyth <cyronize@gmail.com>
2021-09-11 08:04:52 -0400

    change: migrate most of EntityTransformer to mixins

--- src/main/java/club/sk1er/patcher/asm/world/entity/EntityTransformer.java
+++ src/main/java/club/sk1er/patcher/asm/world/entity/EntityTransformer.java
@@ -13,9 +13,9 @@ package club.sk1er.patcher.asm.world.entity;
 
 import club.sk1er.patcher.tweaker.transform.CommonTransformer;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.*;
-
-import java.util.ListIterator;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
 
 public class EntityTransformer implements CommonTransformer {
     /**
@@ -40,117 +40,10 @@ public class EntityTransformer implements CommonTransformer {
         classNode.fields.add(new FieldNode(Opcodes.ACC_PUBLIC, "displayNameCache", "Lnet/minecraft/util/IChatComponent;", null, null));
         for (MethodNode methodNode : classNode.methods) {
             final String methodName = mapMethodName(classNode, methodNode);
-            if (methodNode.name.equals("hasCapability")) {
-                clearInstructions(methodNode);
-                methodNode.instructions.insert(fasterCapabilityCheck());
-            }
-
-            switch (methodName) {
-                case "getBrightnessForRender":
-                case "func_70070_b":
-                    clearInstructions(methodNode);
-                    methodNode.instructions.insert(getFixedBrightness());
-                    break;
-                case "spawnRunningParticles":
-                case "func_174830_Y":
-                    final ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator();
-                    LabelNode ifeq = new LabelNode();
-                    while (iterator.hasNext()) {
-                        final AbstractInsnNode next = iterator.next();
-                        if (next instanceof JumpInsnNode && next.getOpcode() == Opcodes.IFNE) {
-                            methodNode.instructions.insert(next.getNext(), checkOnGround(ifeq));
-                        } else if (next instanceof MethodInsnNode && next.getOpcode() == Opcodes.INVOKEVIRTUAL) {
-                            final String methodInsnName = mapMethodNameFromNode(next);
-                            if (methodInsnName.equals("createRunningParticles") || methodInsnName.equals("func_174808_Z")) {
-                                methodNode.instructions.insert(next.getNext(), ifeq);
-                            }
-                        }
-                    }
-                    break;
-                case "getDisplayName":
-                case "func_145748_c_":
-                    cachePlayerHoverEvents(methodNode);
-                    break;
+            // todo: port this to a mixin
+            if (methodName.equals("getDisplayName") || methodName.equals("func_145748_c_")) {
+                cachePlayerHoverEvents(methodNode);
             }
         }
-    }
-
-    private InsnList checkOnGround(LabelNode ifeq) {
-        InsnList list = new InsnList();
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/entity/Entity", "field_70122_E", "Z"));
-        list.add(new JumpInsnNode(Opcodes.IFEQ, ifeq));
-        return list;
-    }
-
-    private InsnList getFixedBrightness() {
-        InsnList list = new InsnList();
-        list.add(new TypeInsnNode(Opcodes.NEW, "net/minecraft/util/BlockPos"));
-        list.add(new InsnNode(Opcodes.DUP));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD,
-            "net/minecraft/entity/Entity",
-            "field_70165_t", // posX
-            "D"));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD,
-            "net/minecraft/entity/Entity",
-            "field_70163_u", // posY
-            "D"));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
-            "net/minecraft/entity/Entity",
-            "func_70047_e", // getEyeHeight
-            "()F",
-            false));
-        list.add(new InsnNode(Opcodes.F2D));
-        list.add(new InsnNode(Opcodes.DADD));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD,
-            "net/minecraft/entity/Entity",
-            "field_70161_v", // posZ
-            "D"));
-        list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, "net/minecraft/util/BlockPos", "<init>", "(DDD)V", false));
-        list.add(new VarInsnNode(Opcodes.ASTORE, 2));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD,
-            "net/minecraft/entity/Entity",
-            "field_70170_p", // worldObj
-            "Lnet/minecraft/world/World;"));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 2));
-        list.add(new InsnNode(Opcodes.ICONST_0));
-        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
-            "net/minecraft/world/World",
-            "func_175626_b", // getCombinedLight
-            "(Lnet/minecraft/util/BlockPos;I)I",
-            false));
-        list.add(new InsnNode(Opcodes.IRETURN));
-        return list;
-    }
-
-    private InsnList fasterCapabilityCheck() {
-        InsnList list = new InsnList();
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/entity/Entity", "capabilities", "Lnet/minecraftforge/common/capabilities/CapabilityDispatcher;"));
-        LabelNode labelNode = new LabelNode();
-        list.add(new JumpInsnNode(Opcodes.IFNULL, labelNode));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-        list.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/entity/Entity", "capabilities", "Lnet/minecraftforge/common/capabilities/CapabilityDispatcher;"));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 1));
-        list.add(new VarInsnNode(Opcodes.ALOAD, 2));
-        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
-            "net/minecraftforge/common/capabilities/CapabilityDispatcher",
-            "hasCapability",
-            "(Lnet/minecraftforge/common/capabilities/Capability;Lnet/minecraft/util/EnumFacing;)Z",
-            false));
-        list.add(new JumpInsnNode(Opcodes.IFEQ, labelNode));
-        list.add(new InsnNode(Opcodes.ICONST_1));
-        LabelNode gotoInsn = new LabelNode();
-        list.add(new JumpInsnNode(Opcodes.GOTO, gotoInsn));
-        list.add(labelNode);
-        list.add(new InsnNode(Opcodes.ICONST_0));
-        list.add(gotoInsn);
-        list.add(new InsnNode(Opcodes.IRETURN));
-        return list;
     }
 }
---
+++ src/main/java/club/sk1er/patcher/mixins/EntityMixin_Capability.java
@@ -0,0 +1,24 @@
+package club.sk1er.patcher.mixins;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityDispatcher;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Entity.class)
+public class EntityMixin_Capability {
+
+    @Shadow private CapabilityDispatcher capabilities;
+
+    /**
+     * @author asbyth
+     * @reason Faster capability check
+     */
+    @Overwrite
+    public boolean hasCapability(Capability<?> capability, EnumFacing direction) {
+        return this.capabilities != null && this.capabilities.hasCapability(capability, direction);
+    }
+}
