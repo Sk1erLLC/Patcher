f0bba5a6fe32b1b7cbdd84d6fb095175212b3301
asbyth <cyronize@gmail.com>
2021-09-14 04:49:37 -0400

    change: cleanup fast server join

--- src/main/java/club/sk1er/patcher/mixins/GuiMultiplayerMixin_FastServerJoin.java
+++ src/main/java/club/sk1er/patcher/mixins/GuiMultiplayerMixin_FastServerJoin.java
@@ -5,7 +5,6 @@ import net.minecraft.client.gui.GuiScreen;
 import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,43 +19,11 @@ public abstract class GuiMultiplayerMixin_FastServerJoin {
     private void patcher$joinServer(CallbackInfo ci) {
         // todo: this can select the lan scan entry
         if (GuiScreen.isCtrlKeyDown()) {
-            switch (Keyboard.getEventKey()) {
-                case Keyboard.KEY_1:
-                    patcher$connectToServer(0);
-                    break;
-                case Keyboard.KEY_2:
-                    patcher$connectToServer(1);
-                    break;
-                case Keyboard.KEY_3:
-                    patcher$connectToServer(2);
-                    break;
-                case Keyboard.KEY_4:
-                    patcher$connectToServer(3);
-                    break;
-                case Keyboard.KEY_5:
-                    patcher$connectToServer(4);
-                    break;
-                case Keyboard.KEY_6:
-                    patcher$connectToServer(5);
-                    break;
-                case Keyboard.KEY_7:
-                    patcher$connectToServer(6);
-                    break;
-                case Keyboard.KEY_8:
-                    patcher$connectToServer(7);
-                    break;
-                case Keyboard.KEY_9:
-                    patcher$connectToServer(8);
-                    break;
-                default:
-                    break;
+            int eventKey = Keyboard.getEventKey();
+            if (eventKey >= Keyboard.KEY_1 && eventKey <= Keyboard.KEY_9) {
+                selectServer(eventKey - Keyboard.KEY_1);
+                connectToSelected();
             }
         }
-    }
-
-    @Unique
-    private void patcher$connectToServer(int index) {
-        selectServer(index);
-        connectToSelected();
     }
 }
