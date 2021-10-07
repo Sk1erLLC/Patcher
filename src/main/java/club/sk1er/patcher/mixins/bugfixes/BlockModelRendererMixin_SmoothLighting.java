1a35d9807192c237bf241baf6a8a1178afa9ba7d
LlamaLad7 <l3gomindstorms@gmail.com>
2021-10-07 17:46:31 +0100

    Fix: Fix incorrect target for BlockModelRendererMixin_SmoothLighting.

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/BlockModelRendererMixin_SmoothLighting.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/BlockModelRendererMixin_SmoothLighting.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "net.minecraft.client.renderer.BlockModelRenderer.AmbientOcclusionFace")
+@Mixin(targets = "net.minecraft.client.renderer.BlockModelRenderer$AmbientOcclusionFace")
 public class BlockModelRendererMixin_SmoothLighting {
     @Redirect(method = "updateVertexBrightness", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;isTranslucent()Z", opcode = Opcodes.INVOKEVIRTUAL))
     private boolean patcher$betterSmoothLighting(Block block) {
