ba5d0e2d7b65dddf4ca28c39fe460f0150352c28
LlamaLad7 <l3gomindstorms@gmail.com>
2021-10-08 19:00:19 +0100

    Fix: Work around BlockModelRendererMixin_SmoothLighting not being remapped correctly.

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/BlockModelRendererMixin_SmoothLighting.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/BlockModelRendererMixin_SmoothLighting.java
@@ -1,14 +1,19 @@
 package club.sk1er.patcher.mixins.bugfixes;
 
 import net.minecraft.block.Block;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "net.minecraft.client.renderer.BlockModelRenderer$AmbientOcclusionFace")
 public class BlockModelRendererMixin_SmoothLighting {
-    @Redirect(method = "updateVertexBrightness", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;isTranslucent()Z", opcode = Opcodes.INVOKEVIRTUAL))
+    @Redirect(
+        method = "updateVertexBrightness(Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/block/Block;Lnet/minecraft/util/BlockPos;Lnet/minecraft/util/EnumFacing;[FLjava/util/BitSet;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/block/Block;isTranslucent()Z"
+        )
+    )
     private boolean patcher$betterSmoothLighting(Block block) {
         return !block.isVisuallyOpaque() || block.getLightOpacity() == 0;
     }
