a9a17dad828d51987a6cff8e767975c348b117d0
LlamaLad7 <l3gomindstorms@gmail.com>
2021-10-11 17:14:58 +0100

    Fix: Resolve some Mixin issues.

--- src/main/java/club/sk1er/patcher/mixins/features/GuiContainerMixin_DropModifierKey.java
+++ src/main/java/club/sk1er/patcher/mixins/features/GuiContainerMixin_DropModifierKey.java
@@ -1,6 +1,7 @@
 package club.sk1er.patcher.mixins.features;
 
 import club.sk1er.patcher.Patcher;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.settings.GameSettings;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(GuiContainer.class)
 public class GuiContainerMixin_DropModifierKey {
     @Redirect(method = "keyTyped", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiContainer;isCtrlKeyDown()Z"))
-    private boolean patcher$useModifierKey(boolean original) {
-        return original || GameSettings.isKeyDown(Patcher.instance.getDropModifier());
+    private boolean patcher$useModifierKey() {
+        return GuiScreen.isCtrlKeyDown() || GameSettings.isKeyDown(Patcher.instance.getDropModifier());
     }
 }
--- src/main/java/club/sk1er/patcher/mixins/features/GuiNewChatMixin_TransparentChat.java
+++ src/main/java/club/sk1er/patcher/mixins/features/GuiNewChatMixin_TransparentChat.java
@@ -9,8 +9,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiNewChat.class)
 public abstract class GuiNewChatMixin_TransparentChat extends Gui {
-
-    @Redirect(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiNewChat;drawRect(IIIII)V", opcode = 0))
+    @Redirect(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiNewChat;drawRect(IIIII)V", ordinal = 0))
     private void patcher$transparentChat(int left, int top, int right, int bottom, int color) {
         if (PatcherConfig.transparentChat) return;
         drawRect(left, top, right, bottom, color);
