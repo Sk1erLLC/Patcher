1609f5e937a0d4eb949a162595082884e3317b25
LlamaLad7 <l3gomindstorms@gmail.com>
2021-10-30 14:02:26 +0100

    Fix: resolve map bobbing not working with OptiFine.

--- src/main/java/club/sk1er/patcher/mixins/features/EntityRendererMixin_ViewBobbing.java
+++ src/main/java/club/sk1er/patcher/mixins/features/EntityRendererMixin_ViewBobbing.java
@@ -4,15 +4,29 @@ import club.sk1er.patcher.config.PatcherConfig;
 import club.sk1er.patcher.hooks.EntityRendererHook;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.settings.GameSettings;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin_ViewBobbing {
-    @Redirect(method = "renderHand", at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;viewBobbing:Z", ordinal = 0))
+    @Redirect(
+        method = "renderHand(FI)V",
+        at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;viewBobbing:Z", ordinal = 0)
+    )
     private boolean patcher$mapBobbing(GameSettings instance) {
         return instance.viewBobbing && !EntityRendererHook.hasMap();
+    }
+
+    @Dynamic("OptiFine adds its own version of renderHand")
+    @Redirect(
+        method = "renderHand(FIZZZ)V",
+        at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;viewBobbing:Z", ordinal = 0, remap = true),
+        remap = false
+    )
+    private boolean patcher$mapBobbingOptiFine(GameSettings instance) {
+        return patcher$mapBobbing(instance);
     }
 
     @Redirect(method = "setupCameraTransform", at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;viewBobbing:Z", ordinal = 0))
