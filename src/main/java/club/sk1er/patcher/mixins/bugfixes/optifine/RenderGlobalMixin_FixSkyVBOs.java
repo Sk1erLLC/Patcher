8489dcfb974177b79f6cfeec1a4e49a29df4efa6
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2021-11-29 12:26:43 -0800

    Use slice over ordinal for entire method and improve legibility

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/RenderGlobalMixin_FixSkyVBOs.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/RenderGlobalMixin_FixSkyVBOs.java
@@ -6,17 +6,33 @@ import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(RenderGlobal.class)
 public class RenderGlobalMixin_FixSkyVBOs {
     @Dynamic("OptiFine")
-    @Redirect(method = "renderSky(Lnet/minecraft/client/renderer/WorldRenderer;FZ)V", at = @At(value ="FIELD", target = "net/minecraft/client/renderer/RenderGlobal.renderDistance:I", opcode = Opcodes.GETFIELD))
+    @Redirect(
+        method = "renderSky(Lnet/minecraft/client/renderer/WorldRenderer;FZ)V",
+        at = @At(
+            value ="FIELD",
+            target = "net/minecraft/client/renderer/RenderGlobal.renderDistance:I",
+            opcode = Opcodes.GETFIELD
+        )
+    )
     private int patcher$distanceOverride(RenderGlobal instance) {
         return 256;
     }
 
     @Dynamic("OptiFine")
-    @Redirect(method = "renderSky(FI)V", at = @At(value = "FIELD", target = "net/minecraft/client/renderer/RenderGlobal.vboEnabled:Z", opcode = Opcodes.GETFIELD, ordinal = 3))
+    @Redirect(
+        method = "renderSky(FI)V",
+        slice = @Slice(from = @At(value = "FIELD", target = "net/minecraft/client/settings/GameSettings.renderDistanceChunks:I")),
+        at = @At(
+            value = "FIELD",
+            target = "net/minecraft/client/renderer/RenderGlobal.vboEnabled:Z",
+            ordinal = 0
+        )
+    )
     private boolean patcher$fixVBO(RenderGlobal instance) {
         return false;
     }
