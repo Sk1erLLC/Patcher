d768cc647943b4ed039bfe5814ef7d9bfe9bafeb
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2021-11-29 11:37:34 -0800

    Always return 256 for render distance rather than a local variable

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/RenderGlobalMixin_FixSkyVBOs.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/optifine/RenderGlobalMixin_FixSkyVBOs.java
@@ -5,15 +5,14 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RenderGlobal.class)
 public class RenderGlobalMixin_FixSkyVBOs {
     @Dynamic("OptiFine")
-    @ModifyVariable(method = "renderSky(Lnet/minecraft/client/renderer/WorldRenderer;FZ)V", at = @At("STORE"), ordinal = 2)
-    private int patcher$distanceOverride(int k) {
-        return 384;
+    @Redirect(method = "renderSky(Lnet/minecraft/client/renderer/WorldRenderer;FZ)V", at = @At(value ="FIELD", target = "net/minecraft/client/renderer/RenderGlobal.renderDistance:I", opcode = Opcodes.GETFIELD))
+    private int patcher$distanceOverride(RenderGlobal instance) {
+        return 256;
     }
 
     @Dynamic("OptiFine")
