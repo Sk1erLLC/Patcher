b206777625cdec682c180e21dafd909404b4b18a
asbyth <cyronize@gmail.com>
2021-12-18 00:55:04 -0500

    fix: resolve bossbar height not working in 1.12

--- src/main/java/club/sk1er/patcher/mixins/features/GuiPlayerTabOverlayMixin_BossbarHeight.java
+++ src/main/java/club/sk1er/patcher/mixins/features/GuiPlayerTabOverlayMixin_BossbarHeight.java
@@ -1,29 +1,42 @@
 package club.sk1er.patcher.mixins.features;
 
-// todo: make this work with 1.12
-//#if MC==10809
 import club.sk1er.patcher.config.PatcherConfig;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.entity.boss.BossStatus;
 import net.minecraftforge.client.GuiIngameForge;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//#endif
 import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import org.spongepowered.asm.mixin.Mixin;
+
+//#if MC==10809
+import net.minecraft.entity.boss.BossStatus;
+//#else
+//$$ import net.minecraft.client.Minecraft;
+//$$ import org.spongepowered.asm.mixin.Final;
+//$$ import org.spongepowered.asm.mixin.Shadow;
+//$$ import club.sk1er.patcher.mixins.accessors.GuiBossOverlayAccessor;
+//#endif
 
 @Mixin(GuiPlayerTabOverlay.class)
 public class GuiPlayerTabOverlayMixin_BossbarHeight {
 
-    //#if MC==10809
+    //#if MC==11202
+    //$$ @Shadow @Final
+    //$$ private Minecraft mc;
+    //#endif
+
     @Unique
     private boolean patcher$willShiftDown;
 
     @Inject(method = "renderPlayerlist", at = @At("HEAD"))
     private void patcher$shiftDownHead(CallbackInfo ci) {
+        //#if MC==10809
         this.patcher$willShiftDown = PatcherConfig.tabHeightAllow && BossStatus.bossName != null && BossStatus.statusBarTime > 0 && GuiIngameForge.renderBossHealth;
+        //#else
+        //$$ this.patcher$willShiftDown = PatcherConfig.tabHeightAllow && !((GuiBossOverlayAccessor) mc.ingameGUI.getBossOverlay()).getMapBossInfos().isEmpty();
+        //#endif
         if (this.patcher$willShiftDown) {
             GlStateManager.pushMatrix();
             GlStateManager.translate(0, PatcherConfig.tabHeight, 0);
@@ -36,6 +49,5 @@ public class GuiPlayerTabOverlayMixin_BossbarHeight {
             GlStateManager.popMatrix();
         }
     }
-    //#endif
 }
 
