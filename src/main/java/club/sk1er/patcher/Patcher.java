086fc43578cd298e239312f4c08f429cecbe91a9
asbyth <cyronize@gmail.com>
2022-02-25 05:18:09 -0500

    change: update version

--- src/main/java/club/sk1er/patcher/Patcher.java
+++ src/main/java/club/sk1er/patcher/Patcher.java
@@ -97,7 +97,7 @@ public class Patcher {
     // betas will be "1.x.x+beta-y" / "1.x.x+branch_beta-y"
     // rcs will be 1.x.x+rc-y
     // extra branches will be 1.x.x+branch-y
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.8.1+beta-1";
 
     private final Logger logger = LogManager.getLogger("Patcher");
     private final File logsDirectory = new File(Minecraft.getMinecraft().mcDataDir + File.separator + "logs" + File.separator);
--- src/main/java/club/sk1er/patcher/mixins/features/MinecraftMixin_BetterKeybindHandling.java
+++ src/main/java/club/sk1er/patcher/mixins/features/MinecraftMixin_BetterKeybindHandling.java
@@ -3,9 +3,7 @@ package club.sk1er.patcher.mixins.features;
 import club.sk1er.patcher.config.PatcherConfig;
 import club.sk1er.patcher.hooks.MinecraftHook;
 import net.minecraft.client.Minecraft;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,8 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Minecraft.class)
 public class MinecraftMixin_BetterKeybindHandling {
     //#if MC==10809
-    @Shadow @Final public static boolean isRunningOnMac;
-
     @Inject(method = "setIngameFocus", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/MouseHelper;grabMouseCursor()V"))
     private void patcher$makeKeysReRegister(CallbackInfo ci) {
         if (PatcherConfig.newKeybindHandling && !Minecraft.isRunningOnMac) {
