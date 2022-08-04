ebb780de4d02d210345e9c519ea992f60e6824ba
SkyyWasTaken <coolg0092002@gmail.com>
2022-08-04 04:48:44 -0400

    Remove unnecessary injection Fix a bug where the player could stop all momentum in midair after an exceptionally short fall

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/EntityMixin_SneakFix.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/EntityMixin_SneakFix.java
@@ -8,23 +8,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin_SneakFix {
-    private double yDisplacement;
-
-    @Inject(method = "moveEntity", at = @At(value = "HEAD"))
-    private void moveEntity(
-        //#if MC==11202
-        //$$ MoverType type,
-        //#endif
-        double x, double y, double z, CallbackInfo ci) {
-        this.yDisplacement = y;
-    }
-
     @Redirect(
         method = "moveEntity",
         at = @At(
@@ -34,11 +21,15 @@ public abstract class EntityMixin_SneakFix {
             ordinal = 0
         )
     )
-    private boolean patcher$overrideOnGround(Entity entity) {
+    private boolean patcher$overrideOnGround(Entity entity,
+                                             //#if MC==11202
+                                             //$$ MoverType type,
+                                             //#endif
+                                             double xDisplacement, double yDisplacement) {
         return entity instanceof EntityPlayer
             && !((EntityPlayer) entity).capabilities.isFlying
             && yDisplacement <= 0.0D
             && entity.fallDistance < entity.stepHeight
-            && !entity.worldObj.getCollidingBoundingBoxes(entity, entity.getEntityBoundingBox().offset(0, -entity.stepHeight, 0)).isEmpty();
+            && !entity.worldObj.getCollidingBoundingBoxes(entity, entity.getEntityBoundingBox().offset(0, entity.fallDistance - entity.stepHeight, 0)).isEmpty();
     }
 }
