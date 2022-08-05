4eb25ded74f0667e5a3f8e002c99332167bb3be6
Sychic <47618543+Sychic@users.noreply.github.com>
2022-08-05 13:56:27 -0400

    change: update version

--- src/main/java/club/sk1er/patcher/Patcher.java
+++ src/main/java/club/sk1er/patcher/Patcher.java
@@ -94,7 +94,7 @@ public class Patcher {
     // betas will be "1.x.x+beta-y" / "1.x.x+branch_beta-y"
     // rcs will be 1.x.x+rc-y
     // extra branches will be 1.x.x+branch-y
-    public static final String VERSION = "1.8.2";
+    public static final String VERSION = "1.8.3+rc-1";
 
     private final Logger logger = LogManager.getLogger("Patcher");
     private final File logsDirectory = new File(Minecraft.getMinecraft().mcDataDir + File.separator + "logs" + File.separator);
