f0ba90bf0ee3bc7b59c95d2dc4f3d59799a26d75
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-08-06 22:47:22 +0900

    fix: make transparent chat feature use `WrapWithCondition` instead of `Redirect`

--- src/main/java/club/sk1er/patcher/mixins/features/GuiNewChatMixin_TransparentChat.java
+++ src/main/java/club/sk1er/patcher/mixins/features/GuiNewChatMixin_TransparentChat.java
@@ -1,17 +1,16 @@
 package club.sk1er.patcher.mixins.features;
 
 import club.sk1er.patcher.config.PatcherConfig;
+import gg.essential.lib.mixinextras.injector.WrapWithCondition;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiNewChat.class)
 public abstract class GuiNewChatMixin_TransparentChat extends Gui {
-    @Redirect(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiNewChat;drawRect(IIIII)V", ordinal = 0))
-    private void patcher$transparentChat(int left, int top, int right, int bottom, int color) {
-        if (PatcherConfig.transparentChat) return;
-        drawRect(left, top, right, bottom, color);
+    @WrapWithCondition(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiNewChat;drawRect(IIIII)V", ordinal = 0))
+    private boolean patcher$transparentChat(int left, int top, int right, int bottom, int color) {
+        return !PatcherConfig.transparentChat;
     }
 }
