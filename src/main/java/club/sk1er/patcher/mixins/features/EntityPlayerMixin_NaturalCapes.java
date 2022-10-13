7791cb5d8aed82c206a6f8cdf6b3f2d443d3b154
Befell <failedtestsubjet@gmail.com>
2022-10-13 00:43:28 -0700

    fix: fixed cape collides with armor if wearing and teleporting results in weird cape movement

--- src/main/java/club/sk1er/patcher/mixins/features/EntityPlayerMixin_NaturalCapes.java
+++ src/main/java/club/sk1er/patcher/mixins/features/EntityPlayerMixin_NaturalCapes.java
@@ -3,16 +3,46 @@ package club.sk1er.patcher.mixins.features;
 import club.sk1er.patcher.config.PatcherConfig;
 import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityPlayer.class)
-public class EntityPlayerMixin_NaturalCapes {
+public abstract class EntityPlayerMixin_NaturalCapes extends EntityMixin_NaturalCapes {
+    @Shadow
+    public double prevChasingPosZ;
+
+    @Shadow
+    public double chasingPosX;
+
+    @Shadow
+    public double prevChasingPosX;
+
+    @Shadow
+    public double prevChasingPosY;
+
+    @Shadow
+    public double chasingPosY;
+
+    @Shadow
+    public double chasingPosZ;
+
     @ModifyVariable(method = "onUpdate", ordinal = 3, at = @At("STORE"))
     public double onUpdate(double ori) {
         if (PatcherConfig.naturalCapes) {
             return 999999.0D;
         }
         return ori;
+    }
+
+    @Override
+    protected void setPositionAndRotation$head(double x, double y, double z, float yaw, float pitch, CallbackInfo ci) {
+        if (PatcherConfig.naturalCapes) {
+            this.prevChasingPosY = this.chasingPosY = y;
+            this.prevChasingPosZ = this.chasingPosZ = z;
+            this.prevChasingPosX = this.chasingPosX = x;
+        }
+
     }
 }
