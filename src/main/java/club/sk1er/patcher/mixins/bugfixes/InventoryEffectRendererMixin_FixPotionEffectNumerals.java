0c5f8bbe9bff8c8f2cc76bfc5d8f9ba17d21f832
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-07 02:49:16 +0100

    use @ModifyExpressionValue instead of @Redirect

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/InventoryEffectRendererMixin_FixPotionEffectNumerals.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/InventoryEffectRendererMixin_FixPotionEffectNumerals.java
@@ -2,43 +2,41 @@ package club.sk1er.patcher.mixins.bugfixes;
 
 import club.sk1er.patcher.config.PatcherConfig;
 import club.sk1er.patcher.util.RomanNumerals;
+import gg.essential.lib.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.potion.PotionEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InventoryEffectRenderer.class)
 public class InventoryEffectRendererMixin_FixPotionEffectNumerals {
 
     private int patcher$potionAmplifierLevel;
 
-    @Redirect(
+    @ModifyExpressionValue(
         method = "drawActivePotionEffects",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/potion/PotionEffect;getAmplifier()I", ordinal = 0))
-    public int patcher$skipOriginalCode(PotionEffect potionEffect) {
+    private int patcher$skipOriginalCode(int amplifier) {
         if (PatcherConfig.betterRomanNumerals) {
-            this.patcher$potionAmplifierLevel = potionEffect.getAmplifier();
+            this.patcher$potionAmplifierLevel = amplifier;
             return 1;
         }
-        return potionEffect.getAmplifier();
+        return amplifier;
     }
 
-    @Redirect(
+    @ModifyExpressionValue(
         method = "drawActivePotionEffects",
         at =
         @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/resources/I18n;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;",
             ordinal = 1))
-    public String patcher$addRomanNumeral(String string, Object... objects) {
+    private String patcher$addRomanNumeral(String string) {
         if (PatcherConfig.betterRomanNumerals) {
             if (this.patcher$potionAmplifierLevel > 0) {
                 return RomanNumerals.toRoman(this.patcher$potionAmplifierLevel + 1);
             }
             return "";
         }
-        return I18n.format(string, objects);
+        return string;
     }
 }
