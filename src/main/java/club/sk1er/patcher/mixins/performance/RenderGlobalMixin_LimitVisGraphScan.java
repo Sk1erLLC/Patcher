9c42023711079513b7cecd467d573c755455847b
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-03-14 22:14:27 +0100

    fix: RenderGlobalMixin_LimitVisGraphScan not applying properly (#137)
    * extend the case command fix to tabcompletion options
    
    * add setting to remove the background from chat, only when the chat is closed
    
    * fix RenderGlobalMixin_LimitVisGraphScan not applying properly

--- src/main/java/club/sk1er/patcher/mixins/performance/RenderGlobalMixin_LimitVisGraphScan.java
+++ src/main/java/club/sk1er/patcher/mixins/performance/RenderGlobalMixin_LimitVisGraphScan.java
@@ -3,19 +3,17 @@ package club.sk1er.patcher.mixins.performance;
 import club.sk1er.patcher.ducks.VisGraphExt;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.chunk.VisGraph;
-import net.minecraft.util.EnumFacing;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.Set;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(RenderGlobal.class)
 public class RenderGlobalMixin_LimitVisGraphScan {
-    @Inject(method = "getVisibleFacings", at = @At(value = "NEW", target = "Lnet/minecraft/client/renderer/chunk/VisGraph;<init>()V", shift = At.Shift.AFTER, remap = false), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void patcher$setLimitScan(CallbackInfoReturnable<Set<EnumFacing>> cir, VisGraph visgraph) {
+
+    @ModifyVariable(method = "getVisibleFacings", name = "visgraph", at = @At(value = "STORE", ordinal = 0))
+    private VisGraph patcher$setLimitScan(VisGraph visgraph) {
         ((VisGraphExt) visgraph).patcher$setLimitScan(true);
+        return visgraph;
     }
+
 }
