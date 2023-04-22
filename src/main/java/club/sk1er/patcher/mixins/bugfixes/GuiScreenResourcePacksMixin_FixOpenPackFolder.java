addc8fe0ca18548a9fd4d1d6bd13925b8a61e312
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-04-22 22:41:05 +0200

    fix resource pack folder not opening on windows in 1.12.2 if a space is on the file path

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/GuiScreenResourcePacksMixin_FixOpenPackFolder.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/GuiScreenResourcePacksMixin_FixOpenPackFolder.java
@@ -2,14 +2,20 @@ package club.sk1er.patcher.mixins.bugfixes;
 
 import net.minecraft.client.gui.GuiScreenResourcePacks;
 import org.spongepowered.asm.mixin.Mixin;
-//#if MC==10809
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.injection.At;
+import java.awt.Desktop;
+//#if MC==10809
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.awt.Desktop;
 import java.io.IOException;
+//#endif
+
+//#if MC==11202
+//$$ import org.spongepowered.asm.mixin.injection.Redirect;
+//$$ import net.minecraft.client.renderer.OpenGlHelper;
+//$$ import net.minecraft.util.Util;
+//$$ import java.io.File;
 //#endif
 
 @Mixin(GuiScreenResourcePacks.class)
@@ -29,4 +35,18 @@ public class GuiScreenResourcePacksMixin_FixOpenPackFolder {
         ci.cancel();
     }
     //#endif
+
+    //#if MC==11202
+    //$$ @Redirect(method = "actionPerformed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/OpenGlHelper;openFile(Ljava/io/File;)V"))
+    //$$ private void patcher$fixFolderOpening(File file) {
+    //$$    if (Util.getOSType() == Util.EnumOS.WINDOWS) {
+    //$$        try {
+    //$$            Desktop.getDesktop().open(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks());
+    //$$        } catch (Exception ignored) {}
+    //$$    } else {
+    //$$        OpenGlHelper.openFile(file);
+    //$$    }
+    //$$ }
+    //#endif
+
 }
