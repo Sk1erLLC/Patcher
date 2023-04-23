963c46471c2c93ce410bb66377267e65de95f03a
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-04-23 17:47:53 +0200

    change Redirect with cancellable Inject for the 1.12 fix

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/GuiScreenResourcePacksMixin_FixOpenPackFolder.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/GuiScreenResourcePacksMixin_FixOpenPackFolder.java
@@ -1,21 +1,20 @@
 package club.sk1er.patcher.mixins.bugfixes;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
 import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.injection.At;
-import java.awt.Desktop;
-//#if MC==10809
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.awt.Desktop;
+
+//#if MC==10809
 import java.io.IOException;
 //#endif
 
 //#if MC==11202
-//$$ import org.spongepowered.asm.mixin.injection.Redirect;
-//$$ import net.minecraft.client.renderer.OpenGlHelper;
 //$$ import net.minecraft.util.Util;
-//$$ import java.io.File;
 //#endif
 
 @Mixin(GuiScreenResourcePacks.class)
@@ -37,15 +36,21 @@ public class GuiScreenResourcePacksMixin_FixOpenPackFolder {
     //#endif
 
     //#if MC==11202
-    //$$ @Redirect(method = "actionPerformed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/OpenGlHelper;openFile(Ljava/io/File;)V"))
-    //$$ private void patcher$fixFolderOpening(File file) {
-    //$$    if (Util.getOSType() == Util.EnumOS.WINDOWS) {
-    //$$        try {
-    //$$            Desktop.getDesktop().open(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks());
-    //$$        } catch (Exception ignored) {}
-    //$$    } else {
-    //$$        OpenGlHelper.openFile(file);
-    //$$    }
+    //$$ @Inject(
+    //$$     method = "actionPerformed",
+    //$$     at =
+    //$$     @At(
+    //$$         value = "INVOKE",
+    //$$         target = "Lnet/minecraft/client/renderer/OpenGlHelper;openFile(Ljava/io/File;)V",
+    //$$         shift = At.Shift.BEFORE),
+    //$$     cancellable = true)
+    //$$ private void patcher$fixFolderOpening(CallbackInfo ci) {
+    //$$     if (Util.getOSType() == Util.EnumOS.WINDOWS) {
+    //$$         try {
+    //$$             Desktop.getDesktop().open(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks());
+    //$$         } catch (Exception ignored) {}
+    //$$         ci.cancel();
+    //$$     }
     //$$ }
     //#endif
 
