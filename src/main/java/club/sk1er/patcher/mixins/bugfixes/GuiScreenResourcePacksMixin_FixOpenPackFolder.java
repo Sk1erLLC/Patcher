37b27858f5a660a85c7a732f1c59b073358170e3
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-05-29 02:43:40 +0200

    use UDesktop instead and merge both mixins into one

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/GuiScreenResourcePacksMixin_FixOpenPackFolder.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/GuiScreenResourcePacksMixin_FixOpenPackFolder.java
@@ -1,5 +1,6 @@
 package club.sk1er.patcher.mixins.bugfixes;
 
+import gg.essential.universal.UDesktop;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,51 +8,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.awt.Desktop;
-
-//#if MC==10809
-import java.io.IOException;
-//#endif
-
-//#if MC==11202
-//$$ import net.minecraft.util.Util;
-//#endif
-
 @Mixin(GuiScreenResourcePacks.class)
 public class GuiScreenResourcePacksMixin_FixOpenPackFolder {
-    //#if MC==10809
+
     @Inject(
         method = "actionPerformed",
         at =
         @At(
             value = "INVOKE",
-            target = "Ljava/lang/Runtime;getRuntime()Ljava/lang/Runtime;",
-            ordinal = 1,
-            shift = At.Shift.BEFORE),
+            target = "Lnet/minecraft/client/resources/ResourcePackRepository;getDirResourcepacks()Ljava/io/File;",
+            shift = At.Shift.BY,
+            by = 2),
         cancellable = true)
-    private void patcher$fixFolderOpening(CallbackInfo ci) throws IOException {
-        Desktop.getDesktop().open(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks());
-        ci.cancel();
+    private void patcher$fixFolderOpening(CallbackInfo ci) {
+        if (UDesktop.open(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks())) {
+            ci.cancel();
+        }
     }
-    //#endif
-
-    //#if MC==11202
-    //$$ @Inject(
-    //$$     method = "actionPerformed",
-    //$$     at =
-    //$$     @At(
-    //$$         value = "INVOKE",
-    //$$         target = "Lnet/minecraft/client/renderer/OpenGlHelper;openFile(Ljava/io/File;)V",
-    //$$         shift = At.Shift.BEFORE),
-    //$$     cancellable = true)
-    //$$ private void patcher$fixFolderOpening(CallbackInfo ci) {
-    //$$     if (Util.getOSType() == Util.EnumOS.WINDOWS) {
-    //$$         try {
-    //$$             Desktop.getDesktop().open(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks());
-    //$$         } catch (Exception ignored) {}
-    //$$         ci.cancel();
-    //$$     }
-    //$$ }
-    //#endif
 
 }
