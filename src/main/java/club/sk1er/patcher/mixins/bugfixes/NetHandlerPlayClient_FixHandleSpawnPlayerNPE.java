c0b4509df2b92367c36427512e8732418fbe9413
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-06-11 03:24:19 +0200

    use versions/1.12.2-1.8.9.txt instead of preprocessor stuff inside the mixin

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/NetHandlerPlayClient_FixHandleSpawnPlayerNPE.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/NetHandlerPlayClient_FixHandleSpawnPlayerNPE.java
@@ -2,12 +2,7 @@ package club.sk1er.patcher.mixins.bugfixes;
 
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.network.NetworkPlayerInfo;
-//#if MC==10809
 import net.minecraft.network.play.server.S0CPacketSpawnPlayer;
-//#endif
-//#if MC==11202
-//$$ import net.minecraft.network.play.server.SPacketSpawnPlayer;
-//#endif
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,20 +26,10 @@ public abstract class NetHandlerPlayClient_FixHandleSpawnPlayerNPE {
             target = "Lnet/minecraft/network/PacketThreadUtil;checkThreadAndEnqueue(Lnet/minecraft/network/Packet;Lnet/minecraft/network/INetHandler;Lnet/minecraft/util/IThreadListener;)V",
             shift = At.Shift.AFTER
         ))
-    //#if MC==10809
     private void patcher$fixHandleSpawnPlayerNPE(S0CPacketSpawnPlayer packetIn, CallbackInfo ci) {
         if (this.getPlayerInfo(packetIn.getPlayer()) == null) {
             ci.cancel();
         }
     }
-    //#endif
-
-    //#if MC==11202
-    //$$ private void patcher$fixHandleSpawnPlayerNPE(SPacketSpawnPlayer packetIn, CallbackInfo ci) {
-    //$$     if (this.getPlayerInfo(packetIn.getUniqueId()) == null) {
-    //$$         ci.cancel();
-    //$$     }
-    //$$ }
-    //#endif
 
 }
